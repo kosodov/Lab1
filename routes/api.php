07e4c2399ae5fa0db930743db3f525c082191ba9
kosodov <ploskoviv@mail.ru>
2024-03-05 23:19:42 +0500

    Bug fix3

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\AuthController;
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/user', [AuthController::class, 'getUser']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/tokens', [AuthController::class, 'getTokens']);
+    Route::post('/revoke-tokens', [AuthController::class, 'revokeTokens']);
 });
+
--- routes/web.php
+++ routes/web.php
@@ -2,31 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-use App\Http\Controllers\AuthController;
-
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:api')->group(function () {
-    Route::get('/user', [AuthController::class, 'getUser']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/tokens', [AuthController::class, 'getTokens']);
-    Route::post('/revoke-tokens', [AuthController::class, 'revokeTokens']);
-});
-
 use App\Http\Controllers\InfoController;
 
 Route::get('/info/server', [InfoController::class, 'phpInfo']);
 Route::get('/info/client', [InfoController::class, 'clientInfo']);
 Route::get('/info/database', [InfoController::class, 'databaseInfo']);
-
-
